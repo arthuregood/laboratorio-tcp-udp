e2ac9cdb03600fec0946d654eee4fbc29560a4e4
arthuregood <arthuregood@protonmail.com>
2021-06-10 20:42:04 -0300

    Criação do padrão TCP

--- TCP/TCP_Client.py
+++ TCP/TCP_Client.py
@@ -1,2 +1,14 @@
 # Feito por Arthur Eduardo Good
 # Redes I - 2021/01
+
+import socket
+
+HOST = ''  # IP do host(servidor)
+PORT = 0        # A porta utilizada pelo servidor
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    s.sendall(b'Laboratorio TCP')
+    data = s.recv(1024)
+
+print('Recebido', repr(data))
--- TCP/TCP_Server.py
+++ TCP/TCP_Server.py
@@ -1,2 +1,19 @@
 # Feito por Arthur Eduardo Good
 # Redes I - 2021/01
+
+import socket
+
+HOST = ''  # localhost padrão
+PORT = 0        # Porta a ser utilizada
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Conectado por', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            conn.sendall(data)
